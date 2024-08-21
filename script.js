65ca5d821739c575ef2da9fbda00c5090103efb2
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-08-21 15:47:55 +0800

    Add darken each time hover div

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const container = document.querySelector(`#container`);
 
-let opacity = 0;
-
 for (let i = 0; i < 16; i++) {
     const line = document.createElement(`div`);
     line.classList.add(`line`);
 
     for (let i = 0; i < 16; i++) {
+        let opacity = 100;
+        let random1 = Math.floor(Math.random() * 255);
+        let random2 = Math.floor(Math.random() * 255);
+        let random3 = Math.floor(Math.random() * 255);
         const div = document.createElement(`div`);
         div.classList.add(`grid`);
         div.addEventListener(`mouseenter`, e => {
-            const random1 = Math.floor(Math.random() * 255);
-            const random2 = Math.floor(Math.random() * 255);
-            const random3 = Math.floor(Math.random() * 255);
             div.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
+            div.style.opacity = `${opacity}%`; 
+            opacity -= 10;
         });
         line.append(div);
     };
@@ -37,11 +38,16 @@ clearBtn.addEventListener(`click`, e => {
         line.classList.add(`line`);
     
         for (let i = 0; i < num; i++) {
+            let opacity = 100;
+            let random1 = Math.floor(Math.random() * 255);
+            let random2 = Math.floor(Math.random() * 255);
+            let random3 = Math.floor(Math.random() * 255);
             const div = document.createElement(`div`);
             div.classList.add(`grid`);
             div.addEventListener(`mouseenter`, e => {
-                const random = Math.floor(Math.random() * 255);
-                div.style.backgroundColor = `rgb(${random}, ${random}, ${random})`;
+                div.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
+                div.style.opacity = `${opacity}%`; 
+                opacity -= 10;
             });
             line.append(div);
         };
