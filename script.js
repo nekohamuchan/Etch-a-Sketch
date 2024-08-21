f91aa9f6d3d769bd3255106549dc25796883c8a4
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-08-21 12:41:08 +0800

    Create 16 * 16 square divs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const container = document.querySelector(`#container`);
+
+for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement(`div`);
+    div.style.border = `1px solid black`;
+    div.style.width = `10px`;
+    div.style.height = `10px`;
+    container.append(div);
+};
