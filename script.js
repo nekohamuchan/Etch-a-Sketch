bd4a76ea9b95c881f6f7f8b536191a5eb9f631e4
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-08-21 13:14:12 +0800

    Make div colored after hover

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 16; i++) {
     for (let i = 0; i < 16; i++) {
         const div = document.createElement(`div`);
         div.classList.add(`grid`);
+        div.addEventListener(`mouseenter`, e => div.classList.add(`colored`));
         line.append(div);
     };
     container.append(line);
