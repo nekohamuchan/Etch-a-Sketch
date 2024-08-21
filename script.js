29f157d97199889d11f7b51ee27535e60ec8e42e
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-08-21 15:09:34 +0800

    Make each div different color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(`#container`);
 
+let opacity = 0;
+
 for (let i = 0; i < 16; i++) {
     const line = document.createElement(`div`);
     line.classList.add(`line`);
@@ -7,7 +9,12 @@ for (let i = 0; i < 16; i++) {
     for (let i = 0; i < 16; i++) {
         const div = document.createElement(`div`);
         div.classList.add(`grid`);
-        div.addEventListener(`mouseenter`, e => div.classList.add(`colored`));
+        div.addEventListener(`mouseenter`, e => {
+            const random1 = Math.floor(Math.random() * 255);
+            const random2 = Math.floor(Math.random() * 255);
+            const random3 = Math.floor(Math.random() * 255);
+            div.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
+        });
         line.append(div);
     };
     container.append(line);
@@ -32,7 +39,10 @@ clearBtn.addEventListener(`click`, e => {
         for (let i = 0; i < num; i++) {
             const div = document.createElement(`div`);
             div.classList.add(`grid`);
-            div.addEventListener(`mouseenter`, e => div.classList.add(`colored`));
+            div.addEventListener(`mouseenter`, e => {
+                const random = Math.floor(Math.random() * 255);
+                div.style.backgroundColor = `rgb(${random}, ${random}, ${random})`;
+            });
             line.append(div);
         };
         container.append(line);
